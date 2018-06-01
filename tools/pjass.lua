bc4897e7f79f5409005da84dd101058b2cc55240
nvs <notveryserious@gmail.com>
2018-05-31 17:38:21 -0700

    tools: Refactor PJass handling
    We just pass the command line arguments (including the executable
    itself) to `Shell.execute ()`.  This gives the user more freedom in
    specifying how to invoke PJass.

--- tools/pjass.lua
+++ tools/pjass.lua
@@ -1,37 +1,29 @@
 local Path = require ('map.path')
 local Shell = require ('map.shell')
+local String = require ('map.string')
 
 local PJass = {}
 
-PJass.executable = Path.join ('map', 'external', 'pjass', 'pjass.exe')
+function PJass.run (...)
+	local stdout_path = Path.temporary_path ()
 
--- Runs PJass with the provided `options (table)`. If provided, the `prefix
--- (string)` will be prepended to the command line. All other arguments (the
--- scripts) will be appended to the command line.
---
--- Returns the status of the command, as either `true (boolean)` or `nil`,
--- along with the `output (string)`.
-function PJass.check (prefix, options, ...)
-	local output_log_path = Path.temporary_path ()
-
-	-- The pjass command outputs to `stdout` regardless of its exit status.
+	-- PJass outputs to `stdout` regardless of its exit status.
 	local status = Shell.execute {
-		command = Shell.escape_arguments (prefix,
-			PJass.executable, options, ...),
-		stdout = Shell.escape_argument (output_log_path)
+		command = Shell.escape (...),
+		stdout = Shell.escape (stdout_path)
 	}
 
 	local output = ''
-	local output_log = io.open (output_log_path, 'rb')
+	local stdout = io.open (stdout_path, 'rb')
 
-	if output_log then
-		output = output_log:read ('*a')
-		output_log:close ()
+	if stdout then
+		output = stdout:read ('*a')
+		stdout:close ()
 
-		os.remove (output_log_path)
+		os.remove (stdout_path)
 	end
 
-	return status, output
+	return status, String.trim_right (output)
 end
 
 return PJass
