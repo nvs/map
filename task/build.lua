a5671acd1654a68281f20b005973f85e77c972c2
NVS <notveryserious@gmail.com>
2019-04-30 18:01:18 -0700

    [Build] Add conditional checks
    Instead of performing every build task, we check the settings to
    determine which we should be handling.

--- task/build.lua
+++ task/build.lua
@@ -1,15 +1,29 @@
 local Tasks = require ('map.tasks')
 
 return function (state)
+	local map = state.settings.input.map
+	local build = state.settings.input.build
+
 	local tasks = {
 		'check',
-		'build.environment',
-		'build.w3x.read',
-		'build.inline-strings',
-		'build.user-files',
-		'build.script',
-		'build.w3x.write'
+		'build.environment'
 	}
+
+	if map then
+		table.insert (tasks, 'build.w3x.read')
+		table.insert (tasks, 'build.inline-strings')
+	end
+
+	if build then
+		table.insert (tasks, 'build.user-files')
+	end
+
+	-- The script was checked.  We can attempt to compile.
+	table.insert (tasks, 'build.script')
+
+	if map then
+		table.insert (tasks, 'build.w3x.write')
+	end
 
 	Tasks.add (state, tasks)
 
