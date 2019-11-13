df0cbf92df6a39f96c86b87a57b0dbd7f27acd8d
NVS <notveryserious@gmail.com>
2019-11-12 16:04:47 -0800

    WC3 IDs: Add comment regarding `common.ai`

--- wc3/ids.lua
+++ wc3/ids.lua
@@ -1,6 +1,8 @@
 -- Globals in Warcraft III Lua as of `1.32.0.13594`.
 
 local ai = {
+	-- Only the natives from the `common.ai` are included in the Lua
+	-- environment.
 	natives = {
 		'DebugS',
 		'DebugFI',
