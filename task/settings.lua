0ef9b26c4e5e64e9a19bc1a02ef3dd48ed51943e
NVS <notveryserious@gmail.com>
2019-12-15 22:12:13 -0800

    Settings: Ensure pacakge tables exists

--- task/settings.lua
+++ task/settings.lua
@@ -3,9 +3,11 @@ return function (state)
 	state.settings.map.options = state.settings.map.options or {}
 
 	state.settings.build = state.settings.build or {}
+	state.settings.build.package = state.settings.build.package or {}
 	state.settings.build.options = state.settings.build.options or {}
 
 	state.settings.script = state.settings.script or {}
+	state.settings.script.package = state.settings.script.package or {}
 	state.settings.script.options = state.settings.script.options or {}
 
 	return true
