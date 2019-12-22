8b5cca103b0df6e8bf32e1e413f053c65d7815fd
NVS <notveryserious@gmail.com>
2019-12-21 22:34:49 -0800

    W3S: Refactor code
    For whatever reason I failed to recognize that the format used in 1.32
    is `2`.  Yeah, that simplifies things.  Plus, a new style for the code.
    It is faster too.

--- file/war3map/w3s.lua
+++ file/war3map/w3s.lua
@@ -14,69 +14,60 @@ local flags = {
 	[0x08] = 'music'
 }
 
-function W3S.unpack (input, version)
-	local position
-
-	local function unpack (options)
-		local values = { string.unpack ('<' .. options, input, position) }
-		position = values [#values]
-		return table.unpack (values, 1, #values - 1)
-	end
-
-	local function unpack_flags (option)
-		return Flags.unpack (flags, unpack (option))
-	end
+function W3S.unpack (input)
+	local unpack = string.unpack
+	local format, count, position = unpack ('< i4 i4', input)
+	assert (format == 1 or format == 2)
 
 	local output = {
-		version = unpack ('i4')
+		format = format
 	}
 
-	for index = 1, unpack ('i4') do
-		local sound = {}
-
-		sound.name = unpack ('z')
-		sound.path = unpack ('z')
-		sound.effect = unpack ('z')
-		sound.flags = unpack_flags ('i4')
-		sound.fade = {
-			in_rate = unpack ('i4'),
-			out_rate = unpack ('i4')
-		}
-		sound.volume = unpack ('i4')
-		sound.pitch = unpack ('f')
-		sound.unknown_A = unpack ('f')
-		sound.unknown_B = unpack ('i4')
-		sound.channel = unpack ('i4')
-		sound.distance = {
-			minimum = unpack ('f'),
-			maximum = unpack ('f'),
-			cutoff = unpack ('f')
-		}
-		sound.cone = {
-			angles = {
-				inside = unpack ('f'),
-				outside = unpack ('f'),
-			},
-			volume = {
-				outside = unpack ('i4')
-			},
-			orientation = {
-				x = unpack ('f'),
-				y = unpack ('f'),
-				z = unpack ('f')
+	for index = 1, count do
+		local sound = {
+			fade = {},
+			distance = {},
+			cone = {
+				angles = {},
+				volume = {},
+				orientation = {}
 			}
 		}
 
-		if version.minor >= 32 then
-			-- Seems to be used when generating the script.
-			sound.script = {
-				name = unpack ('z'),
-				label = unpack ('z'),
-				path = unpack ('z')
-			}
+		sound.name,
+		sound.path,
+		sound.effect,
+		sound.flags,
+		sound.fade.in_rate,
+		sound.fade.out_rate,
+		sound.volume,
+		sound.pitch,
+		sound.unknown_A,
+		sound.unknown_B,
+		sound.channel,
+		sound.distance.minimum,
+		sound.distance.maximum,
+		sound.distance.cutoff,
+		sound.cone.angles.inside,
+		sound.cone.angles.outside,
+		sound.cone.volume.outside,
+		sound.cone.orientation.x,
+		sound.cone.orientation.y,
+		sound.cone.orientation.z,
+		position = unpack (
+			'< z z z i4 i4 i4 i4 f f i4 i4 f f f f f i4 f f f',
+			input, position)
 
-			-- Rather than make assumptions, just lump it all together.
-			sound.unknown_C = unpack ('c18')
+		sound.flags = Flags.unpack (flags, sound.flags)
+
+		if format == 2 then
+			sound.script = {}
+
+			sound.script.name,
+			sound.script.label,
+			sound.script.path,
+			sound.unknown_C,
+			position = unpack ('z z z c18', input, position)
 		end
 
 		output [index] = sound
@@ -87,50 +78,47 @@ function W3S.unpack (input, version)
 	return output
 end
 
-function W3S.pack (input, version)
+function W3S.pack (input)
 	assert (type (input) == 'table')
-	assert (type (version) == 'table')
 
 	local output = {}
+	local pack = string.pack
+	local format = input.format or 2
+	assert (format == 1 or format == 2)
 
-	local function pack (options, ...)
-		output [#output + 1] = string.pack ('<' .. options, ...)
-	end
-
-	local function pack_flags (option, value)
-		pack (option, Flags.pack (flags, value))
-	end
-
-	pack ('i4', input.version or 1)
-	pack ('i4', #input)
+	output [#output + 1] = pack ('i4 i4', format, #input)
 
 	for _, sound in ipairs (input) do
-		pack ('z', sound.name)
-		pack ('z', sound.path)
-		pack ('z', sound.effect)
-		pack_flags ('i4', sound.flags)
-		pack ('i4', sound.fade.in_rate)
-		pack ('i4', sound.fade.out_rate)
-		pack ('i4', sound.volume)
-		pack ('f', sound.pitch)
-		pack ('f', sound.unknown_A)
-		pack ('i4', sound.unknown_B)
-		pack ('i4', sound.channel)
-		pack ('f', sound.distance.minimum)
-		pack ('f', sound.distance.maximum)
-		pack ('f', sound.distance.cutoff)
-		pack ('f', sound.cone.angles.inside)
-		pack ('f', sound.cone.angles.outside)
-		pack ('i4', sound.cone.volume.outside)
-		pack ('f', sound.cone.orientation.x)
-		pack ('f', sound.cone.orientation.y)
-		pack ('f', sound.cone.orientation.z)
+		output [#output + 1] = pack (
+			'< z z z i4 i4 i4 i4 f f i4 i4 f f f f f i4 f f f',
+			sound.name,
+			sound.path,
+			sound.effect,
+			Flags.pack (flags, sound.flags),
+			sound.fade.in_rate,
+			sound.fade.out_rate,
+			sound.volume,
+			sound.pitch,
+			sound.unknown_A,
+			sound.unknown_B,
+			sound.channel,
+			sound.distance.minimum,
+			sound.distance.maximum,
+			sound.distance.cutoff,
+			sound.cone.angles.inside,
+			sound.cone.angles.outside,
+			sound.cone.volume.outside,
+			sound.cone.orientation.x,
+			sound.cone.orientation.y,
+			sound.cone.orientation.z)
 
-		if version.minor >= 32 then
-			pack ('z', sound.script.name)
-			pack ('z', sound.script.label)
-			pack ('z', sound.script.path)
-			pack ('c18', sound.unknown_C)
+		if format == 2 then
+			output [#output + 1] = pack (
+				'z z z c18',
+				sound.script.name,
+				sound.script.label,
+				sound.script.path,
+				sound.unknown_C)
 		end
 	end
 
