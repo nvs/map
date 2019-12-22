7286f45ee3aaec2483f7c368967de924b52d7726
NVS <notveryserious@gmail.com>
2019-12-22 13:20:25 -0800

    W3I: Refactor code
    General performance improvements.  Perhaps a hit to readability, but I
    can live with it.  And a few field names have been changed.

--- file/war3map/w3i.lua
+++ file/war3map/w3i.lua
@@ -6,7 +6,6 @@ end
 local Bits = require ('map.file.bits')
 local Flags = require ('map.file.flags')
 
--- Deals with the `war3map.w3i`.
 local W3I = {}
 
 local map_flags = {
@@ -47,249 +46,284 @@ local formats = {
 }
 
 function W3I.unpack (input)
-	local position
+	assert (type (input) == 'string')
 
-	local function unpack (options)
-		local values = { string.unpack ('<' .. options, input, position) }
-		position = values [#values]
-		return table.unpack (values, 1, #values - 1)
-	end
+	local unpack = string.unpack
+	local count
 
-	local function unpack_bits (option)
-		return Bits.unpack (option, unpack (option))
-	end
+	local format,
+		position = unpack ('< i4', input)
 
-	local function unpack_map_flags (option)
-		return Flags.unpack (map_flags, unpack (option))
-	end
-
-	local function unpack_force_flags (option)
-		return Flags.unpack (force_flags, unpack (option))
-	end
-
-	local format = unpack ('i4')
 	assert (formats [format])
 
-	local output = {}
+	local output = {
+		format = format,
+		map = {},
+		camera = { {}, {}, {}, {} },
+		margins = {},
+		dimensions = {},
+		loading = {},
+		prologue = {}
+	}
 
-	output.format = format
-	output.saves = unpack ('i4')
-	output.editor = unpack ('i4')
+	output.saves,
+	output.editor,
+	position = unpack ('< i4 i4', input, position)
 
 	if format >= 28 then
-		output.version = {
-			major = unpack ('i4'),
-			minor = unpack ('i4'),
-			patch = unpack ('i4'),
-			build = unpack ('i4')
-		}
+		output.version = {}
+
+		output.version.major,
+		output.version.minor,
+		output.version.patch,
+		output.version.build,
+		position = unpack ('< i4 i4 i4 i4', input, position)
 	end
 
-	output.map = {
-		name = unpack ('z'),
-		author = unpack ('z'),
-		description = unpack ('z'),
-		recommended = unpack ('z'),
-	}
+	output.map.name,
+	output.map.author,
+	output.map.description,
+	output.map.recommended,
+	output.camera [1].x,
+	output.camera [1].y,
+	output.camera [2].x,
+	output.camera [2].y,
+	output.camera [3].x,
+	output.camera [3].y,
+	output.camera [4].x,
+	output.camera [4].y,
+	output.margins.left,
+	output.margins.right,
+	output.margins.top,
+	output.margins.bottom,
+	output.dimensions.width,
+	output.dimensions.height,
+	output.map.flags,
+	output.tileset,
+	position = unpack (
+		'< z z z z f f f f f f f f i4 i4 i4 i4 i4 i4 I4 c1',
+		input, position)
 
-	output.camera = {}
-
-	for index = 1, 4 do
-		output.camera [index] = {
-			x = unpack ('f'),
-			y = unpack ('f')
-		}
-	end
-
-	output.margins = {
-		left = unpack ('i4'),
-		right = unpack ('i4'),
-		top = unpack ('i4'),
-		bottom = unpack ('i4')
-	}
-
-	output.dimensions = {
-		width = unpack ('i4'),
-		height = unpack ('i4')
-	}
-
-	output.map.flags = unpack_map_flags ('I4')
-	output.tileset = unpack ('c1')
+	output.map.flags = Flags.unpack (map_flags, output.map.flags)
 
 	if format == 18 then
-		output.campaign = {
-			background = unpack ('i4')
-		}
+		output.campaign = {}
 
-		output.loading = {
-			text = unpack ('z'),
-			title = unpack ('z'),
-			subtitle = unpack ('z'),
-			background = unpack ('i4')
-		}
-
-		output.prologue = {
-			text = unpack ('z'),
-			title = unpack ('z'),
-			subtitle = unpack ('z')
-		}
+		output.campaign.background,
+		output.loading.text,
+		output.loading.title,
+		output.loading.subtitle,
+		output.loading.background,
+		output.prologue.text,
+		output.prologue.title,
+		output.prologue.subtitle,
+		position = unpack ('< i4 z z z i4 z z z', input, position)
 	else
-		output.loading = {
-			background = unpack ('i4'),
-			model = unpack ('z'),
-			text = unpack ('z'),
-			title = unpack ('z'),
-			subtitle = unpack ('z')
-		}
-
-		output.game_data = unpack ('i4')
-
-		output.prologue = {
-			model = unpack ('z'),
-			text = unpack ('z'),
-			title = unpack ('z'),
-			subtitle = unpack ('z')
-		}
-
 		output.fog = {
-			index = unpack ('i4'),
-			z = {
-				start = unpack ('f'),
-				finish = unpack ('f')
-			},
-			density = unpack ('f'),
-			color = {
-				red = unpack ('B'),
-				green = unpack ('B'),
-				blue = unpack ('B'),
-				alpha = unpack ('B')
-			}
+			z = {},
+			color = {}
+		}
+		output.environment = {
+			water = {}
 		}
 
-		output.environment = {
-			weather = unpack ('c4'),
-			sound = unpack ('z'),
-			light = unpack ('c1'),
-			water = {
-				red = unpack ('B'),
-				green = unpack ('B'),
-				blue = unpack ('B'),
-				alpha = unpack ('B')
-			}
-		}
+		output.loading.background,
+		output.loading.model,
+		output.loading.text,
+		output.loading.title,
+		output.loading.subtitle,
+		output.game_data,
+		output.prologue.model,
+		output.prologue.text,
+		output.prologue.title,
+		output.prologue.subtitle,
+		output.fog.index,
+		output.fog.z.start,
+		output.fog.z.finish,
+		output.fog.density,
+		output.fog.color.red,
+		output.fog.color.green,
+		output.fog.color.blue,
+		output.fog.color.alpha,
+		output.environment.weather,
+		output.environment.sound,
+		output.environment.light,
+		output.environment.water.red,
+		output.environment.water.green,
+		output.environment.water.blue,
+		output.environment.water.alpha,
+		position = unpack (
+			'< i4 z z z z i4 z z z z i4 f f f B B B B c4 z c1 B B B B',
+			input, position)
 	end
 
 	if format >= 28 then
-		output.is_lua = unpack ('i4') == 1
+		output.script,
+		position = unpack ('< i4', input, position)
 	end
 
-	if format == 31 then
-		output.quality = unpack ('i4')
-		output.game_data_version = unpack ('i4')
+	if format >= 31 then
+		output.graphics,
+		output.game_data_version,
+		position = unpack ('< i4 i4', input, position)
 	end
 
 	output.players = {}
 
-	for index = 1, unpack ('i4') do
+	count,
+	position = unpack ('< i4', input, position)
+
+	for index = 1, count do
 		local player = {
-			index = unpack ('i4'),
-			type = unpack ('i4'),
-			race = unpack ('i4'),
-			start = {
-				fixed = unpack ('i4')
-			},
-			name = unpack ('z'),
-			ally = {},
-			enemy = {}
+			start = {},
+			ally = {}
 		}
-
-		player.start.x = unpack ('f')
-		player.start.y = unpack ('f')
-
-		player.ally.low = unpack_bits ('I4')
-		player.ally.high = unpack_bits ('I4')
-
-		if format == 31 then
-			player.enemy.low = unpack_bits ('I4')
-			player.enemy.high = unpack_bits ('I4')
-		end
-
 		output.players [index] = player
+
+		player.index,
+		player.type,
+		player.race,
+		player.start.fixed,
+		player.name,
+		player.start.x,
+		player.start.y,
+		player.ally.low,
+		player.ally.high,
+		position = unpack ('< i4 i4 i4 i4 z f f I4 I4', input, position)
+
+		player.ally.low = Bits.unpack ('I4', player.ally.low)
+		player.ally.high = Bits.unpack ('I4', player.ally.high)
+
+		if format >= 31 then
+			player.enemy = {}
+
+			player.enemy.low,
+			player.enemy.high,
+			position = unpack ('< I4 I4', input, position)
+
+			player.enemy.low = Bits.unpack ('I4', player.enemy.low)
+			player.enemy.high = Bits.unpack ('I4', player.enemy.high)
+		end
 	end
 
 	output.forces = {}
 
-	for index = 1, unpack ('i4') do
-		output.forces [index] = {
-			flags = unpack_force_flags ('I4'),
-			players = unpack_bits ('I4'),
-			name = unpack ('z')
-		}
+	count,
+	position = unpack ('< i4', input, position)
+
+	for index = 1, count do
+		local force = {}
+		output.forces [index] = force
+
+		force.flags,
+		force.players,
+		force.name,
+		position = unpack ('< I4 I4 z', input, position)
+
+		force.flags = Flags.unpack (force_flags, force.flags)
+		force.players = Bits.unpack ('I4', force.players)
 	end
 
 	output.upgrades = {}
 
-	for index = 1, unpack ('i4') do
-		output.upgrades [index] = {
-			players = unpack_bits ('I4'),
-			id = unpack ('c4'),
-			level = unpack ('i4'),
-			availability = unpack ('i4')
-		}
+	count,
+	position = unpack ('< i4', input, position)
+
+	for index = 1, count do
+		local upgrade = {}
+		output.upgrades [index] = upgrade
+
+		upgrade.players,
+		upgrade.id,
+		upgrade.level,
+		upgrade.availability,
+		position = unpack ('< I4 c4 i4 i4', input, position)
+
+		upgrade.players = Bits.unpack ('I4', upgrade.players)
 	end
 
 	output.tech = {}
 
-	for index = 1, unpack ('i4') do
-		output.tech [index] = {
-			players = unpack_bits ('I4'),
-			id = unpack ('c4')
-		}
+	count,
+	position = unpack ('< i4', input, position)
+
+	for index = 1, count do
+		local tech = {}
+		output.tech [index] = tech
+
+		tech.players,
+		tech.id,
+		position = unpack ('< I4 c4', input, position)
+
+		tech.players = Bits.unpack ('I4', tech.players)
 	end
 
 	output.units = {}
 
-	for index = 1, unpack ('i4') do
-		local unit = {
-			index = unpack ('i4'),
-			name = unpack ('z'),
-			columns = unpack ('i4'),
+	count,
+	position = unpack ('< i4', input, position)
+
+	for index = 1, count do
+		local units = {
 			rows = {}
 		}
+		output.units [index] = units
 
-		unit.type = { unpack (('i4'):rep (unit.columns)) }
+		units.index,
+		units.name,
+		units.columns,
+		position = unpack ('< i4 z i4', input, position)
 
-		for row = 1, unpack ('i4') do
-			unit.rows [row] = {
-				chance = unpack ('i4'),
-				id = { unpack (('c4'):rep (unit.columns)) }
-			}
+		local columns = units.columns
+		units.type = { unpack (('i4'):rep (columns), input, position) }
+		position = table.remove (units.type)
+
+		count,
+		position = unpack ('< i4', input, position)
+
+		for row = 1, count do
+			units.rows [row] = {}
+			row = units.rows [row]
+
+			row.chance,
+			position = unpack ('< i4', input, position)
+
+			row.id = { unpack (('c4'):rep (columns), input, position) }
+			position = table.remove (row.id)
 		end
-
-		output.units [index] = unit
 	end
 
 	if format >= 25 then
 		output.item_tables = {}
 
-		for index = 1, unpack ('i4') do
-			local item_table = {
-				index = unpack ('i4'),
-				name = unpack ('z')
-			}
+		count,
+		position = unpack ('< i4', input, position)
 
-			for set = 1, unpack ('i4') do
-				item_table [set] = {}
+		for table = 1, count do
+			local item_table = {}
+			output.item_tables [table] = item_table
 
-				for item = 1, unpack ('i4') do
-					item_table [set] [item] = {
-						chance = unpack ('i4'),
-						id = unpack ('c4')
-					}
+			item_table.index,
+			item_table.name,
+			count,
+			position = unpack ('< i4 z i4', input, position)
+
+			for set = 1, count do
+				local items = {}
+				item_table [set] = items
+
+				count,
+				position = unpack ('< i4', input, position)
+
+				for index = 1, count do
+					local item = {}
+					items [index] = item
+
+					item.chance,
+					item.id,
+					position = unpack ('< i4 c4', input, position)
 				end
 			end
-
-			output.item_tables [index] = item_table
 		end
 	end
 
@@ -301,184 +335,192 @@ end
 function W3I.pack (input)
 	assert (type (input) == 'table')
 
+	local pack = string.pack
+
 	local output = {}
-
-	local function pack (options, ...)
-		output [#output + 1] = string.pack ('<' .. options, ...)
-	end
-
-	local function pack_bits (option, value)
-		pack (option, Bits.pack (value))
-	end
-
-	local function pack_map_flags (option, value)
-		pack (option, Flags.pack (map_flags, value))
-	end
-
-	local function pack_force_flags (option, value)
-		pack (option, Flags.pack (force_flags, value))
-	end
-
-	local format = input.format
+	local format = input.format or 31
 	assert (formats [format])
 
-	pack ('i4', input.format)
-	pack ('i4', input.saves)
-	pack ('i4', input.editor)
+	output [#output + 1] = pack (
+		'< i4 i4 i4',
+		format,
+		input.saves,
+		input.editor)
 
 	if format >= 28 then
-		pack ('i4', input.version.major)
-		pack ('i4', input.version.minor)
-		pack ('i4', input.version.patch)
-		pack ('i4', input.version.build)
+		output [#output + 1] = pack (
+			'< i4 i4 i4 i4',
+			input.version.major,
+			input.version.minor,
+			input.version.patch,
+			input.version.build)
 	end
 
-	pack ('z', input.map.name)
-	pack ('z', input.map.author)
-	pack ('z', input.map.description)
-	pack ('z', input.map.recommended)
-
-	for index = 1, 4 do
-		pack ('f', input.camera [index].x)
-		pack ('f', input.camera [index].y)
-	end
-
-	pack ('i4', input.margins.left)
-	pack ('i4', input.margins.right)
-	pack ('i4', input.margins.top)
-	pack ('i4', input.margins.bottom)
-
-	pack ('i4', input.dimensions.width)
-	pack ('i4', input.dimensions.height)
-
-	pack_map_flags ('I4', input.map.flags)
-	pack ('c1', input.tileset)
+	output [#output + 1] = pack (
+		'< z z z z f f f f f f f f i4 i4 i4 i4 i4 i4 I4 c1',
+		input.map.name,
+		input.map.author,
+		input.map.description,
+		input.map.recommended,
+		input.camera [1].x,
+		input.camera [1].y,
+		input.camera [2].x,
+		input.camera [2].y,
+		input.camera [3].x,
+		input.camera [3].y,
+		input.camera [4].x,
+		input.camera [4].y,
+		input.margins.left,
+		input.margins.right,
+		input.margins.top,
+		input.margins.bottom,
+		input.dimensions.width,
+		input.dimensions.height,
+		Flags.pack (map_flags, input.map.flags),
+		input.tileset)
 
 	if format == 18 then
-		pack ('i4', input.campaign.background)
-
-		pack ('z', input.loading.text)
-		pack ('z', input.loading.title)
-		pack ('z', input.loading.subtitle)
-		pack ('i4', input.loading.background)
-
-		pack ('z', input.prologue.text)
-		pack ('z', input.prologue.title)
-		pack ('z', input.prologue.subtitle)
+		output [#output + 1] = pack (
+		 '< i4 z z z i4 z z z',
+		input.campaign.background,
+		input.loading.text,
+		input.loading.title,
+		input.loading.subtitle,
+		input.loading.background,
+		input.prologue.text,
+		input.prologue.title,
+		input.prologue.subtitle)
 	else
-		pack ('i4', input.loading.background)
-		pack ('z', input.loading.model)
-		pack ('z', input.loading.text)
-		pack ('z', input.loading.title)
-		pack ('z', input.loading.subtitle)
-
-		pack ('i4', input.game_data)
-
-		pack ('z', input.prologue.model)
-		pack ('z', input.prologue.text)
-		pack ('z', input.prologue.title)
-		pack ('z', input.prologue.subtitle)
-
-		pack ('i4', input.fog.index)
-		pack ('f', input.fog.z.start)
-		pack ('f', input.fog.z.finish)
-		pack ('f', input.fog.density)
-		pack ('B', input.fog.color.red)
-		pack ('B', input.fog.color.green)
-		pack ('B', input.fog.color.blue)
-		pack ('B', input.fog.color.alpha)
-
-		pack ('c4', input.environment.weather)
-		pack ('z', input.environment.sound)
-		pack ('c1', input.environment.light)
-		pack ('B', input.environment.water.red)
-		pack ('B', input.environment.water.green)
-		pack ('B', input.environment.water.blue)
-		pack ('B', input.environment.water.alpha)
+		output [#output + 1] = pack (
+			'< i4 z z z z i4 z z z z i4 f f f B B B B c4 z c1 B B B B',
+			input.loading.background,
+			input.loading.model,
+			input.loading.text,
+			input.loading.title,
+			input.loading.subtitle,
+			input.game_data,
+			input.prologue.model,
+			input.prologue.text,
+			input.prologue.title,
+			input.prologue.subtitle,
+			input.fog.index,
+			input.fog.z.start,
+			input.fog.z.finish,
+			input.fog.density,
+			input.fog.color.red,
+			input.fog.color.green,
+			input.fog.color.blue,
+			input.fog.color.alpha,
+			input.environment.weather,
+			input.environment.sound,
+			input.environment.light,
+			input.environment.water.red,
+			input.environment.water.green,
+			input.environment.water.blue,
+			input.environment.water.alpha)
 	end
 
 	if format >= 28 then
-		pack ('i4', input.is_lua and 1 or 0)
+		output [#output + 1] = pack ('< i4', input.script)
 	end
 
-	if format == 31 then
-		pack ('i4', input.quality)
-		pack ('i4', input.game_data_version)
+	if format >= 31 then
+		output [#output + 1] = pack (
+			'< i4 i4',
+			input.graphics,
+			input.game_data_version)
 	end
 
-	pack ('i4', #input.players)
+	output [#output + 1] = pack ('< i4', #input.players)
 
 	for _, player in ipairs (input.players) do
-		pack ('i4', player.index)
-		pack ('i4', player.type)
-		pack ('i4', player.race)
-		pack ('i4', player.start.fixed)
-		pack ('z', player.name)
-		pack ('f', player.start.x)
-		pack ('f', player.start.y)
-		pack_bits ('I4', player.ally.low)
-		pack_bits ('I4', player.ally.high)
+		output [#output + 1] = pack (
+			'< i4 i4 i4 i4 z f f I4 I4',
+			player.index,
+			player.type,
+			player.race,
+			player.start.fixed,
+			player.name,
+			player.start.x,
+			player.start.y,
+			Bits.pack (player.ally.low),
+			Bits.pack (player.ally.high))
 
 		if format == 31 then
-			pack_bits ('I4', player.enemy.low)
-			pack_bits ('I4', player.enemy.high)
+			output [#output + 1] = pack (
+				'< I4 I4',
+				Bits.pack (player.enemy.low),
+				Bits.pack (player.enemy.high))
 		end
 	end
 
-	pack ('i4', #input.forces)
+	output [#output + 1] = pack ('< i4', #input.forces)
 
 	for _, force in ipairs (input.forces) do
-		pack_force_flags ('I4', force.flags)
-		pack_bits ('I4', force.players)
-		pack ('z', force.name)
+		output [#output + 1] = pack (
+			'< I4 I4 z',
+			Flags.pack (force_flags, force.flags),
+			Bits.pack (force.players),
+			force.name)
 	end
 
-	pack ('i4', #input.upgrades)
+	output [#output + 1] = pack ('< i4', #input.upgrades)
 
 	for _, upgrade in ipairs (input.upgrades) do
-		pack_bits ('I4', upgrade.players)
-		pack ('c4', upgrade.id)
-		pack ('i4', upgrade.level)
-		pack ('i4', upgrade.availability)
+		output [#output + 1] = pack (
+			'< I4 c4 i4 i4',
+			Bits.pack (upgrade.players),
+			upgrade.id,
+			upgrade.level,
+			upgrade.availability)
 	end
 
-	pack ('i4', #input.tech)
+	output [#output + 1] = pack ('< i4', #input.tech)
 
 	for _, tech in ipairs (input.tech) do
-		pack_bits ('I4', tech.players)
-		pack ('c4', tech.id)
+		output [#output + 1] = pack (
+			'< I4 c4',
+			Bits.pack (tech.players),
+			tech.id)
 	end
 
-	pack ('i4', #input.units)
+	output [#output + 1] = pack ('< i4', #input.units)
 
 	for _, units in ipairs (input.units) do
-		pack ('i4', units.index)
-		pack ('z', units.name)
-		pack ('i4', units.columns)
-		pack (('i4'):rep (units.columns), table.unpack (units.type))
-
-		pack ('i4', #units.rows)
+		output [#output + 1] = pack (
+			'< i4 z i4 i4' .. ('i4'):rep (units.columns),
+			units.index,
+			units.name,
+			units.columns,
+			table.unpack (units.type),
+			#units.rows)
 
 		for _, row in ipairs (units.rows) do
-			pack ('i4', row.chance)
-			pack (('c4'):rep (units.columns), table.unpack (row.id))
+			output [#output + 1] = pack (
+				'< i4' .. ('c4'):rep (units.columns),
+				row.chance,
+				table.unpack (row.id))
 		end
 	end
 
 	if format >= 25 then
-		pack ('i4', #input.item_tables)
+		output [#output + 1] = pack ('< i4', #input.item_tables)
 
 		for _, item_table in ipairs (input.item_tables) do
-			pack ('i4', item_table.index)
-			pack ('z', item_table.name)
-			pack ('i4', #item_table)
+			output [#output + 1] = pack (
+				'< i4 z i4',
+				item_table.index,
+				item_table.name,
+				#item_table)
 
 			for _, set in ipairs (item_table) do
-				pack ('i4', #set)
+				output [#output + 1] = pack ('< i4', #set)
 
 				for _, item in ipairs (set) do
-					pack ('i4', item.chance)
-					pack ('c4', item.id)
+					output [#output + 1] = pack (
+						'< i4 c4',
+						item.chance,
+						item.id)
 				end
 			end
 		end
