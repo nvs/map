82d400d50b2393944de3463d6875e4eaf340d527
NVS <notveryserious@gmail.com>
2020-01-21 14:57:44 -0800

    WC3 IDs: No change in 1.32.0.14411

--- wc3/ids.lua
+++ wc3/ids.lua
@@ -1,4 +1,4 @@
--- Globals in Warcraft III Lua as of `1.32.0.14391`.
+-- Globals in Warcraft III Lua as of `1.32.0.14411`.
 
 local ai = {
 	-- Only the natives from the `common.ai` are included in the Lua
